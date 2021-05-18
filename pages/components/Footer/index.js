91635654a7045f1a62b12e7d50b1548acd9c30b6
girordo <tsgiroldo@gmail.com>
2021-05-18 15:27:46 -0300

    feat: setting up footer

--- pages/components/Footer/index.js
+++ pages/components/Footer/index.js
@@ -3,32 +3,32 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex-col  w-full h-24 border-t">
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className="my-6 flex items-center justify-center"
+          href="https://netlify.com"
           target="_blank"
           rel="noopener noreferrer">
           Powered by
           <img src="/netlify.svg" alt="Netlify Logo" className="h-6 ml-2" />
         </a>
+        <div className="m-2 flex items-center justify-center">
+          <p>
+            Made with
+            <span aria-label="Heart Purple Emoji" role="img" className="mx-2">
+              💜
+            </span>
+            by
+          </p>
+          <a
+            className="flex-col mx-2"
+            href="https://github.com/girordo"
+            target="_blank"
+            rel="noreferrer">
+            /girordo
+          </a>
+        </div>
       </footer>
-      <div>
-        <p className="mx-2">
-          Made with
-          <span aria-label="Heart Purple Emoji" role="img" className="mx-2">
-            💜
-          </span>
-          by
-        </p>
-        <a
-          className="flex-col mx-2"
-          href="https://github.com/girordo"
-          target="_blank"
-          rel="noreferrer">
-          /girordo
-        </a>
-      </div>
     </>
   );
 };
