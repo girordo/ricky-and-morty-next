5d646f584344826efe2ad0623d1b363070548ae7
girordo <tsgiroldo@gmail.com>
2021-09-07 18:54:18 -0300

    :art: | font semibold

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className="w-full h-24 sticky top-0 bg-gray-50 flex items-center justify-center border-b mb-10 shadow-lg">
+    <nav className="w-full h-24 sticky top-0 bg-gray-50 flex items-center justify-center border-b mb-10 shadow-lg font-semibold">
       <Link href="/characters">Personagens</Link>
       <Link href="/locations">Localizações</Link>
     </nav>
