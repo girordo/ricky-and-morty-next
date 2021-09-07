ad6e18c5e76db261252fe85da5859b921abd130a
girordo <tsgiroldo@gmail.com>
2021-09-07 14:51:12 -0300

    :art: | auqele monte de mudança desnecessária só pra cumprir com a promessa de codar todos os dias de setembro

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <nav className="w-full h-24 sticky top-0 bg-gray-50 flex items-center justify-center border-b mb-10 shadow-lg">
-      Aqui é o cabeçalho treta
+      <Link href="/characters">Personagens</Link>
+      <Link href="/locations">Localizações</Link>
     </nav>
   );
 };
